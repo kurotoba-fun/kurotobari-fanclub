25db9f86e04c910dd9a1ffa4f7075a5bb4346724
mimmy <mikako.the.world@gmail.com>
2026-07-04 14:38:46 +0900

    Use root baseurl for Cloudflare Pages

--- scripts/cloudflare-pages-build.sh
+++ scripts/cloudflare-pages-build.sh
@@ -7,4 +7,4 @@ export LC_CTYPE=en_US.UTF-8
 export RUBYOPT="${RUBYOPT:+$RUBYOPT }-EUTF-8"
 
 bundle exec jekyll clean
-bundle exec jekyll build --baseurl "${JEKYLL_BASEURL:-/mainworld}"
+bundle exec jekyll build --baseurl "${JEKYLL_BASEURL:-}"
