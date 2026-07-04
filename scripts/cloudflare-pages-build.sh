14e4aa4e038bf08be3656fa809220217feb781a7
mimmy <mikako.the.world@gmail.com>
2026-07-04 14:28:33 +0900

    Configure Cloudflare Pages build

---
+++ scripts/cloudflare-pages-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export RUBYOPT="${RUBYOPT:+$RUBYOPT }-EUTF-8"
+
+bundle exec jekyll clean
+bundle exec jekyll build --baseurl "${JEKYLL_BASEURL:-/mainworld}"
